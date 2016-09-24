16dfa768abd1d6dd5bfab684044bd59eccbde56e
richardgong1987 <909253305@qq.com>
2016-09-24 23:01:33 +0800

    submit

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -3,39 +3,60 @@
     <img class="logo" src="../assets/vux_logo.png">
     <h1>{{ msg }}</h1>
   </div>
-  <group title="cell demo">
-    <cell title="Vux" value="Cool" is-link></cell>
+  <group>
+    <calendar title="Date Picker"></calendar>
+    <xnumber title="Number" :value=0 :min=0 :max=10 @on-change="change"></xnumber>
   </group>
+
+  <tabbar>
+    <tabbaritem><img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+      <span slot="label">Wechat</span></tabbaritem>
+    <tabbaritem><img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+      <span slot="label">Wechat</span></tabbaritem>
+    <tabbaritem><img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+      <span slot="label">Wechat</span></tabbaritem>
+  </tabbar>
 </template>
 
 <script>
-import Group from 'vux/src/components/group'
-import Cell from 'vux/src/components/cell'
+  import Group from 'vux/src/components/group'
+  import calendar from 'vux/src/components/calendar'
+  import xnumber from 'vux/src/components/x-number'
+  import tabbar from 'vux/src/components/tabbar/tabbar.vue'
+  import tabbaritem from 'vux/src/components/tabbar/tabbar-item.vue'
 
-export default {
-  components: {
-    Group,
-    Cell
-  },
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+  export default {
+    components: {
+      Group,
+      calendar,
+      xnumber,
+      tabbar,
+      tabbaritem
+    },
+    data () {
+      return {
+        // note: changing this line won't causes changes
+        // with hot-reload because the reloaded component
+        // preserves its current state and we are modifying
+        // its initial state.
+        msg: 'Hello World!'
+      }
+    },
+    methods: {
+      change () {
+
+      }
     }
   }
-}
 </script>
 
 <style>
-.vux-demo {
-  text-align: center;
-}
+  .vux-demo {
+    text-align: center;
+  }
 
-.logo {
-  width: 100px;
-  height: 100px
-}
+  .logo {
+    width: 100px;
+    height: 100px
+  }
 </style>
